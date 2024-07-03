dac9d54cd49114e9a77cf92eb5c5ceae34e946aa
Hamza Imranist <88247831+hamzaatalif@users.noreply.github.com>
2024-07-03 12:46:38 +0500

    Update add-css.js

--- add-css.js
+++ add-css.js
@@ -1,10 +1,9 @@
-
 (function() {
-    var _0x5e3d = ['createElement', 'link', 'rel', 'stylesheet', 'href', 'aHR0cHM6Ly9naXRodWIuY29tL2hhbXphYXRhbGlmL3B1enpsZS1yZXBvL2Jsb2IvMzE4YmQxOTU1NmM0YWVkNmYyYTA5MGQ3MzhkNWMxOTc0NmFmNDVmZi9oaWRkZW4tc3R5bGUuY3Nz', 'atob', 'appendChild', 'head'];
+    var _0x5e3d = ['createElement', 'link', 'rel', 'stylesheet', 'href', 'https://raw.githubusercontent.com/hamzaatalif/puzzle-repo/main/hidden-style.css', 'appendChild', 'head'];
     (function(_0x10b5e5, _0x4cde52) {
         var _0x562d45 = function(_0x3ef5c1) {
             while (--_0x3ef5c1) {
-                _0x10b5e5['push'](_0x10b5e5['shift']());
+                _0x10b5e5.push(_0x10b5e5.shift());
             }
         };
         _0x562d45(++_0x4cde52);
@@ -17,7 +16,7 @@
     (function() {
         var _0x2e1d30 = document[_0x1dd7('0x0')](_0x1dd7('0x1'));
         _0x2e1d30[_0x1dd7('0x2')] = _0x1dd7('0x3');
-        _0x2e1d30[_0x1dd7('0x4')] = atob(_0x1dd7('0x5'));
+        _0x2e1d30[_0x1dd7('0x4')] = _0x1dd7('0x5');
         document[_0x1dd7('0x6')][_0x1dd7('0x7')](_0x2e1d30);
     })();
 })();
